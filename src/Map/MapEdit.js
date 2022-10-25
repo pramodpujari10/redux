ca235c7b5959ffd766cd889d81798b1c318906e0
Pramod Pujari <pramodpujari10@gmail.com>
2022-10-25 10:17:29 +0530

    multiple path added.

--- src/Map/MapEdit.js
+++ src/Map/MapEdit.js
@@ -4,7 +4,6 @@ import {
   useLoadScript,
   Marker,
   Polyline,
-  ScriptLoaded,
 } from "@react-google-maps/api";
 import "./map.css";
 
@@ -25,14 +24,30 @@ function Map() {
   };
 
   const path = [
-    { lat: 18.5204, lng: 73.8567 },
-    { lat: 19.076, lng: 72.8777 },
+    [
+      { lat: 18.5204, lng: 73.8567 },
+      { lat: 19.076, lng: 72.8777 },
+    ],
+    [
+      { lat: 18.7204, lng: 73.9567 },
+      { lat: 19.094, lng: 74.748 },
+      { lat: 17.68, lng: 74.018 },
+    ],
 
     // { lat: 37.772, lng: -122.214 },
     // { lat: 21.291, lng: -157.821 },
     // { lat: -18.142, lng: 178.431 },
     // { lat: -27.467, lng: 153.027 },
   ];
+  // const path = [
+  //   { lat: 18.5204, lng: 73.8567 },
+  //   { lat: 19.076, lng: 72.8777 },
+
+  //   // { lat: 37.772, lng: -122.214 },
+  //   // { lat: 21.291, lng: -157.821 },
+  //   // { lat: -18.142, lng: 178.431 },
+  //   // { lat: -27.467, lng: 153.027 },
+  // ];
 
   const options = {
     strokeColor: "#FF0000",
@@ -43,7 +58,8 @@ function Map() {
     clickable: false,
     draggable: false,
     editable: false,
-    visible: true,
+    // visible: true,
+    visible: btnStatus,
     radius: 30000,
     paths: [
       { lat: 18.5204, lng: 73.8567 },
@@ -82,72 +98,26 @@ function Map() {
         ) : (
           <></>
         )}
-        {btnStatus ? (
+        {path.map((item, index) => {
+          return (
+            <div key={index}>
+              {" "}
+              <Polyline onLoad={onLoad} path={item} options={options} />
+            </div>
+          );
+        })}
+        {/* <Polyline onLoad={onLoad} path={path} options={options} /> */}
+        {/* {btnStatus ? (
           <Polyline onLoad={onLoad} path={path} options={options} />
         ) : (
           <></>
-        )}
+        )} */}
       </GoogleMap>
       <div id="floating-panel">
-        {/* <input id="remove-line" type="button" value="Remove line" />
-        <input id="add-line" type="button" value="Restore line" /> */}
         <button className="btn" onClick={() => setBtnStatus(!btnStatus)}>
-          {btnStatus ? "Hide" : "Show"}
+          {btnStatus ? "Hide path" : "Show path"}
         </button>
       </div>
     </>
   );
 }
-
-//   const mapContainerStyle = {
-//     height: "400px",
-//     width: "800px",
-//   };
-
-//   const center = {
-//     lat: 0,
-//     lng: -180,
-//   };
-
-//   const onLoad = (polyline) => {
-//     console.log("polyline: ", polyline);
-//   };
-
-//   const path = [
-//     { lat: 37.772, lng: -122.214 },
-//     { lat: 21.291, lng: -157.821 },
-//     { lat: -18.142, lng: 178.431 },
-//     { lat: -27.467, lng: 153.027 },
-//   ];
-
-//   const options = {
-//     strokeColor: "#FF0000",
-//     strokeOpacity: 0.8,
-//     strokeWeight: 2,
-//     fillColor: "#FF0000",
-//     fillOpacity: 0.35,
-//     clickable: false,
-//     draggable: false,
-//     editable: false,
-//     visible: true,
-//     radius: 30000,
-//     paths: [
-//       { lat: 37.772, lng: -122.214 },
-//       { lat: 21.291, lng: -157.821 },
-//       { lat: -18.142, lng: 178.431 },
-//       { lat: -27.467, lng: 153.027 },
-//     ],
-//     zIndex: 1,
-//   };
-
-//   // <ScriptLoaded>
-//   <GoogleMap
-//     id="marker-example"
-//     mapContainerStyle={mapContainerStyle}
-//     zoom={2}
-//     center={center}
-//   >
-//     <Polyline onLoad={onLoad} path={path} options={options} />
-//   </GoogleMap>;
-//   // </ScriptLoaded>;
-// }
